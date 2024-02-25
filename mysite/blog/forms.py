218ca59787b960ba2101f1361dcbe38314358b80
User <viktorianika28key@gmail.com>
2024-02-25 10:19:52 +0200

    add login

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -5,3 +5,16 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name','email','body')
+
+class LoginForm(forms.Form):
+    username=forms.CharField(widget=forms.TextInput(attrs={
+        "id":"inputLogin",
+        "class":"form-control",
+        "placeholder":"login",
+    }))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={
+        "type": "password",
+        "id":"inputPassword",
+        "class":"form-control",
+        "placeholder":"0000",
+    }))
